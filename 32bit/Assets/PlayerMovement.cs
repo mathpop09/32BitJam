b102b12ee3e690f5858628043be1077f1dc5290d
SKT T1 IU <32501438+mathpop09@users.noreply.github.com>
2020-12-19 23:09:49 -1000

    sprite movement + turn
    sprite turns and camera has been adjusted

--- 32bit/Assets/PlayerMovement.cs
+++ 32bit/Assets/PlayerMovement.cs
@@ -5,24 +5,45 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 0.1f;
+    private Vector3 RightRot;
+    private Vector3 LeftRot;
+    private bool turnRightMode = true; 
+    public int turnSpeed = 1;
     // Start is called before the first frame update
     void Start()
     {
-        
+        RightRot = this.transform.localRotation.eulerAngles;
+        LeftRot = RightRot + new Vector3(0, 180, 0);
     }
-
+    
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        Debug.Log(Mathf.Abs(Quaternion.Dot(transform.localRotation, Quaternion.Euler(LeftRot))));
+        if (turnRightMode == false && (Quaternion.Angle(transform.localRotation, Quaternion.Euler(LeftRot))) > 0.0000001)
+        {
+            Debug.Log("Inside");
+            Debug.Log(LeftRot);
+            Debug.Log(transform.localEulerAngles);
+            this.transform.localEulerAngles -= new Vector3(0, turnSpeed, 0);
+            Debug.Log(Mathf.Abs(Quaternion.Dot(transform.localRotation, Quaternion.Euler(LeftRot))));
+        }
+        else if (turnRightMode == true && (Quaternion.Angle(transform.localRotation, Quaternion.Euler(RightRot))) > 0.0000001)
+        {
+            this.transform.localEulerAngles += new Vector3(0, turnSpeed, 0);
+        }
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             WalkLeft();
-            Debug.Log("Go");
+            Debug.Log("Left");
+            turnRightMode = false;
         }
-        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             WalkRight();
-            Debug.Log("Go");
+            Debug.Log("Right");
+            turnRightMode = true; 
         }
     }
 
---
+++ 32bit/Assets/cameraController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraController : MonoBehaviour
+{
+
+    private Vector3 playerInit;
+    private Vector3 playerNow;
+    public GameObject player;
+
+    private Vector3 CameraInit;
+
+    private void Start()
+    {
+        playerInit = player.transform.position;
+        CameraInit = transform.position;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        playerNow = player.transform.position;
+        Vector3 Move = playerInit - playerNow;
+        transform.position = CameraInit - Move; 
+    }
+}
