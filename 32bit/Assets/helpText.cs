d674269b6c5f6a97b3f6c1cccfb4de4900f4977f
SKT T1 IU <32501438+mathpop09@users.noreply.github.com>
2020-12-21 17:30:38 -1000

    Papercraft Style Tree Complete + New Portal assett
    Above + Canvas was added as UI

---
+++ 32bit/Assets/helpText.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class helpText : MonoBehaviour
+{
+    public GameObject textHold;
+    public GameObject actualText;
+
+    private float visibilityTime = 5.0f;
+    private float timer = 0.0f; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime; 
+        if (timer > visibilityTime)
+        {
+            textHold.SetActive(false); 
+            actualText.SetActive(false);
+        }
+    }
+}
