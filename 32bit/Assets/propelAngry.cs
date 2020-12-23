cd9cfc4090203fd8b33795d58e39a1d824a5773f
SKT T1 IU <32501438+mathpop09@users.noreply.github.com>
2020-12-22 16:06:29 -1000

    continued ornament level building

---
+++ 32bit/Assets/propelAngry.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class propelAngry : MonoBehaviour
+{
+    public float moveSpeed = 1.0f; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        //GetComponent<Rigidbody>().velocity = new Vector3 (moveSpeed, 0.0f, 0.0f);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position += new Vector3(moveSpeed, 0.0f, 0.0f);
+    }
+}
