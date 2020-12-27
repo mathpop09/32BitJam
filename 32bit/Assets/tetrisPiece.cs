6a286465b8eb6be0de8599560484d2a7400562b6
SKT T1 IU <32501438+mathpop09@users.noreply.github.com>
2020-12-26 17:14:00 -1000

    blocks drop properly in present scene
    drops block properly with sound

--- 32bit/Assets/tetrisPiece.cs
+++ 32bit/Assets/tetrisPiece.cs
@@ -6,31 +6,45 @@ public class tetrisPiece : MonoBehaviour
 {
     public int dropOrder;
     private Vector3 InitialPosition;
-    private MeshRenderer thisSprite;
+    private MeshRenderer[] childRenderers;
+    private Transform[] childTransforms;
+    private bool notVisible = true;
     //timer check for when the piece starts moving;
-    public float waitTime;
+    private float waitTime;
     private float waitTimer = 0.0f;
     //timer check for each unit of distance where the piece moves
-    public float timerUnit;
+    private float timerUnit = 0.5f;
     private float timerMove;
     // Start is called before the first frame update
+
+    private AudioSource pieceAudio;
+    bool soundPitchUp = false;
     void Start()
     {
+        pieceAudio = this.GetComponent<AudioSource>();
+
         InitialPosition = transform.position;
         transform.position += new Vector3(0.0f, 24.0f, 0.0f);
-        thisSprite = this.GetComponent<MeshRenderer>();
-        thisSprite.enabled = false;
+        //childRenderers = this.GetComponentInChildren<MeshRenderer>();
+        childTransforms = GetComponentsInChildren<Transform>();
+        childRenderers = GetComponentsInChildren<MeshRenderer>();
+        childRenderers[0].enabled = false;
+        childRenderers[1].enabled = false;
+        childRenderers[2].enabled = false;
+        childRenderers[3].enabled = false; 
 
-        waitTime = dropOrder * 3.0f;
+        waitTime = dropOrder * 7.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
         //make sprite visible below a certain position
-        makeVisible();
         if (waitTimer >= waitTime)
         {
+            if (notVisible == true) 
+                makeVisible();
+
             movePiece();
         }
         else
@@ -41,10 +55,11 @@ public class tetrisPiece : MonoBehaviour
 
     void makeVisible()
     {
-        if (InitialPosition.y <= 10)
-        {
-            thisSprite.enabled = true;
-        }
+        childRenderers[0].enabled = true;
+        childRenderers[1].enabled = true;
+        childRenderers[2].enabled = true;
+        childRenderers[3].enabled = true;
+        notVisible = false;
     }
 
     void movePiece()
@@ -54,6 +69,17 @@ public class tetrisPiece : MonoBehaviour
             timerMove += Time.deltaTime;
             if (timerMove >= timerUnit)
             {
+                if (soundPitchUp == false)
+                {
+                    soundPitchUp = true;
+                    pieceAudio.pitch += 0.1f;
+                }
+                else
+                {
+                    soundPitchUp = false;
+                    pieceAudio.pitch -= 0.1f;
+                }
+                pieceAudio.Play();
                 timerMove = 0;
                 this.transform.position -= new Vector3(0.0f, 2.0f, 0.0f);
             }
