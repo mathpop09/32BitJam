67b4e694f366c9468807271201e64e5359490189
SKT T1 IU <32501438+mathpop09@users.noreply.github.com>
2020-12-27 14:32:00 -1000

    lava decor in last level

--- 32bit/Assets/lava.cs
+++ 32bit/Assets/lava.cs
@@ -4,15 +4,59 @@ using UnityEngine;
 
 public class lava : MonoBehaviour
 {
+    private float bobLevel;
+
+
+    private Vector3 ogPosition;
     // Start is called before the first frame update
+    private bool goDown = true;
+    private int increments = 0;
+    private int maxIncs = 60;
+
+    private bool started = false;
+    private float bobStartTime;
+    private float bobTimer = 0.0f;
     void Start()
     {
-        
+        ogPosition = this.transform.position;
+        bobLevel = Random.Range(0.3f, 0.6f);
+        bobStartTime = Random.Range(0.0f, 2.0f);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (started == false)
+        {
+            bobTimer += Time.deltaTime;
+            if (bobTimer >= bobStartTime)
+            {
+                started = true;
+            }
+        }
+        if (started == true)
+        {
+            if (goDown == true && increments <= maxIncs)
+            {
+                this.transform.position -= new Vector3(0.0f, bobLevel / maxIncs, 0.0f);
+                increments++;
+            }
+            else if (goDown == true)
+            {
+                increments = 0;
+                goDown = false;
+            }
+            else if (goDown == false && increments <= maxIncs)
+            {
+                this.transform.position += new Vector3(0.0f, bobLevel / maxIncs, 0.0f);
+                increments++;
+            }
+            else if (goDown == false)
+            {
+                increments = 0;
+                goDown = true;
+                bobLevel = Random.Range(0.1f, 0.5f);
+            }
+        }
     }
 }
---
+++ 32bit/Assets/swayingLava.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class swayingLava : MonoBehaviour
+{
+    private Vector3 swayLevel;
+
+    // Start is called before the first frame update
+    private bool goDown = true;
+    private int increments = 0;
+    private int maxIncs = 60;
+    // Start is called before the first frame update
+    void Start()
+    {
+        swayLevel = Random.insideUnitSphere * 2;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (goDown == true && increments <= maxIncs)
+        {
+            this.transform.position -= new Vector3(swayLevel.x / maxIncs, 0.0f, swayLevel.z / maxIncs);
+            increments++;
+        }
+        else if (goDown == true)
+        {
+            increments = 0;
+            goDown = false;
+        }
+        else if (goDown == false && increments <= maxIncs)
+        {
+            this.transform.position += new Vector3(swayLevel.x / maxIncs, 0.0f, swayLevel.z / maxIncs);
+            increments++;
+        }
+        else if (goDown == false)
+        {
+            increments = 0;
+            goDown = true;
+            swayLevel = Random.insideUnitSphere * 2;
+        }
+    }
+}
